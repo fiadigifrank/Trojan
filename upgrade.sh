809e756b82fb865e1ca3478f8077b4ce3f18a51c
Frank.ku <frank.ku@fiadigi.com>
2020-06-15 22:52:22 +0800

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -4,7 +4,7 @@ green='\033[0;32m'
 yellow='\033[0;33m'
 plain='\033[0m'
 [ ! ${1} ] && echo -e "${red}错误!: 请输入版本号${plain}" && exit 1
-getOldVer=$(/usr/bin/trojan-go/trojan-go -version |grep Trojan-Go |head -n 1|cut -d ' ' -f 2)
+getOldVer=$(/etc/trojan/bin/trojan-go -version |grep Trojan-Go |head -n 1|cut -d ' ' -f 2)
 getServiceList=$(systemctl list-units --state=running|grep trojan |cut -d ' ' -f 1)
 echo -e "${green}当前trojan版本:${getOldVer}${plain}"
 for i in ${getServiceList}; do systemctl stop ${i} && echo -e "${yellow}停止trojan服务:${i}${plain}" ; done
@@ -12,7 +12,7 @@ for i in ${getServiceList}; do systemctl stop ${i} && echo -e "${yellow}停止tr
 mkdir /tmp/trojan
 wget -O /tmp/trojan/trojan.zip https://github.com/p4gefau1t/trojan-go/releases/download/${1}/trojan-go-linux-amd64.zip && echo -e "${green}下载新版本:${1}${plain}"
 unzip /tmp/trojan/*.zip -d /tmp/trojan/
-cp -f /tmp/trojan/trojan-go /tmp/trojan/geo* /usr/bin/trojan-go && echo -e "${green}新版本覆盖安装完毕${plain}"
-getNewVer=$(/usr/bin/trojan-go/trojan-go -version |grep Trojan-Go |head -n 1|cut -d ' ' -f 2)
+cp -f /tmp/trojan/trojan-go /tmp/trojan/geo* /etc/trojan/bin && echo -e "${green}新版本覆盖安装完毕${plain}"
+getNewVer=$(/etc/trojan/bin/trojan-go -version |grep Trojan-Go |head -n 1|cut -d ' ' -f 2)
 echo -e "${green}当前trojan版本:${getNewVer}${plain}"
 for i in ${getServiceList}; do systemctl start ${i} && echo -e "${yellow}启动trojan服务:${i}${plain}" ; done
